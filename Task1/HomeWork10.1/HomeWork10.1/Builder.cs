43cc7f232b1f9b24666f90aac672e09b53667e13
Azer Kerimov <azerkerimov03@gmail.com>
2021-12-05 17:22:09 +0400

    hi

---
+++ Task1/HomeWork10.1/HomeWork10.1/Builder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork10._1
+{
+    class Builder
+    {
+        public string Word;
+
+        public Builder(string word)
+        {
+            this.Word = word;
+            string result = string.Empty;
+            StringBuilder sb = new StringBuilder();
+            StringBuilder rb = new StringBuilder();
+            StringBuilder fb = new StringBuilder();
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                sb.Append(word[i]);
+              
+            }
+
+             
+
+          for (int i = 0; i < sb.Length; i++)
+            {
+                if (sb[i] == ' ')
+                {
+                    break;
+                }
+
+                rb.Append(sb[i]);
+                
+            }
+            
+
+
+            for (int i = 0; i < word.Length ; i++)
+            {
+                if (word[i] == ' ')
+                {
+                    break;
+                }
+                result += word[i];
+            }
+            for (int i = result.Length-1; i >= 0; i--)
+            {
+                fb.Append(result[i]);
+            }
+            
+            Console.WriteLine(fb + " " + rb);
+
+
+
+        }
+        
+
+
+    }
+}
---
+++ Task1/HomeWork10.1/HomeWork10.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HomeWork10._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Builder str1 = new Builder("Hello World");
+            
+        }
+    }
+}
